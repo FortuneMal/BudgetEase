2f73109940be8ff1cd0123dfa2c4c84f2d20ed0d
FortuneMal <144704344+FortuneMal@users.noreply.github.com>
2025-07-04 12:51:05 +0000

    backend work

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = process.env.MONGO_URI;
+mongoose.connect(db)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+const users = require('./routes/users');
+const expenses = require('./routes/expenses');
+
+app.use('/api/users', users);
+app.use('/api/expenses', expenses);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
